df92d1fe68dae99db7bc99d5e30ab5b4b024b11b
lloydkev000 <lloydkevin631@gmail.com>
2025-02-20 13:56:55 -0500

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+
+        var reverseGrade = new GradeComparator().reversed();
+        var age = new AgeComparator();
+
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("James", 10, 15));
+        students.add(new Student("Jake", 11, 16));
+        students.add(new Student("Charlie", 9, 14));
+
+        students.sort(reverseGrade.thenComparing(age));
+        System.out.println(students);
+
+
+    }
 }
